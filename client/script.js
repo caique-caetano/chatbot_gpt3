bab1661fb6d8c3a42742ff5f0890e1e1cc9148db
caique-caetano <caiquecaetano07@gmail.com>
2023-03-14 10:21:32 -0300

    Alteração para API online

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("hhttps://chatbot-gpt3-1bca.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
